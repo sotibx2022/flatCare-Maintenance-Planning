c4b9f5c341e035d1bb2c2589790ecbe76de4809b
Binayaraj Soti <105714812+sotibx2022@users.noreply.github.com>
2024-07-25 00:04:04 +0300

    Update route.tsx
    extra / in signup route deleted.

--- src/app/api/customer/signup/route.tsx
+++ src/app/api/customer/signup/route.tsx
@@ -86,7 +86,7 @@ export async function POST(req: NextRequest) {
     });
     await savedCustomer.save();
     // generate verification Link
-    const verificationLink = `${process.env.WEBSITE_URL}/customer/verify-customer?userId=${newCustomer._id}&verifyToken=${verifyToken.toString()}`;
+    const verificationLink = `${process.env.WEBSITE_URL}customer/verify-customer?userId=${newCustomer._id}&verifyToken=${verifyToken.toString()}`;
     // Send Email to user
     // Call Email Verification Template
     const registerEmail = EmailVerificationTemplate(verificationLink);
