a4972e6f9c13cf1c79e8cf38a77a6357a1731e87
Binaya <sbinayaraj@gmail.com>
2025-09-09 23:52:13 +0300

    landing page spacing issues resolved

--- src/app/landingpage/softwareFeatures/SoftwareFeatures.tsx
+++ src/app/landingpage/softwareFeatures/SoftwareFeatures.tsx
@@ -23,7 +23,7 @@ const features = [
 ];
 export const SoftwareFeatures = () => {
   return (
-    <section className="py-20 px-4">
+    <section className="py-10 px-4">
       <div className="container mx-auto max-w-6xl">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {features.map((feature) => (
