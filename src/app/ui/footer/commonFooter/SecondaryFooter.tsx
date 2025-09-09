882091394785e9b391c8e621191161b31b892878
Binayaraj Soti <105714812+sotibx2022@users.noreply.github.com>
2025-09-09 23:04:11 +0300

    Update SecondaryFooter.tsx

--- src/app/ui/footer/commonFooter/SecondaryFooter.tsx
+++ src/app/ui/footer/commonFooter/SecondaryFooter.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 const SecondaryFooter = () => {
     return (
         <div className='secondaryFooterContainer'>
-            <p className='container'>Copyright by FlatCare Maintenance Planning ©2024 All rights reserved </p>
+            <p className='container'>© 2024 FlatCare Maintenance Planning. All rights reserved.</p>
+
         </div>
     )
 }
